9d1a4400ef94f47bf319ced96c4cfdfa98c3ba47
Kassandra Perch <kperch89@gmail.com>
2011-11-02 21:45:23 +0000

    Working. Ready for talk

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.configure('production', function(){
 
 // Routes
 
-app.listen(process.env.C9_PORT || 8080);
+app.listen(process.env.C9_PORT || process.env.PORT || 3000);
 
 //
 // Add socket.io to our app
@@ -37,33 +37,33 @@ var io = require('socket.io').listen(app);
 // Init canvas array of 4 * pixel count octets in rgba structure
 //
 var canvas = {};
-canvas.width = 10;
-canvas.height = 10;
+canvas.width = 100;
+canvas.height = 100;
 canvas.data = {};
 canvas.data.length = canvas.height * canvas.width * 4;
 for(var i = 0; i < canvas.data.length; i++) {
-  canvas.data[i] = 255;
-  if(i%4===2) {
-    canvas.data[i] = 0;
+  canvas.data[i] = 0;
+}
+
+function drawRect(raster) {
+  var index = 4 * ((raster.top || 0) * canvas.width + (raster.left || 0));
+  for(var i = 0; i < raster.data.length;) {
+    canvas.data[index+i] = raster.data[i]
+    canvas.data[index+i+1] = raster.data[i+1]
+    canvas.data[index+i+2] = raster.data[i+2]
+    canvas.data[index+i+3] = raster.data[i+3]
+    i+=4;
+    if((i/4) % raster.width === 0) {
+      index += canvas.width * 4 - raster.width * 4;
+    }
   }
 }
 
 io.sockets.on('connection', function (socket) {
-  //console.error(socket);
-  socket.emit('raster', canvas);
-  socket.on('raster', function(raster) {
-    socket.broadcast.emit('raster', raster);
-    var index = 4 * (raster.top * canvas.width + raster.left);
-    for(var i = 0; i < raster.data.length;) {
-      canvas.data[index+i%raster.width] = raster.data[i]
-      canvas.data[index+i%raster.width+1] = raster.data[i+1]
-      canvas.data[index+i%raster.width+2] = raster.data[i+2]
-      canvas.data[index+i%raster.width+3] = raster.data[i+3]
-      i+=4;
-      if((i/4) % raster.width === 0) {
-        index += canvas.width * 4 - raster.width * 4;
-      }
-    }
+  socket.emit('client-init', canvas);
+  socket.on('server-raster', function(raster) {
+    socket.broadcast.emit('client-raster', raster);
+    drawRect(raster);
   });
 });
 
